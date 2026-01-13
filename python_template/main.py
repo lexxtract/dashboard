c93ea024f25be72fe69092b8895ced9a02fe5185
DKormann <dominikormann@outlook.com>
2026-01-13 17:21:39 +0100

    build

--- python_template/main.py
+++ python_template/main.py
@@ -3,25 +3,18 @@ import requests
 import asyncio
 import json
 
+OPENROUTER_API_KEY = json.load(open("./.env.json"))["api_key"]
 
 db_url = "https://maincloud.spacetimedb.com"
 dbname = "lexxtract"
-
 token : str = None
 
 async def db_request(path: str, method: str, body: str = None):
   global token
-  if not token:
-    token = requests.post(f"{db_url}/v1/identity", json={}).json()["token"] 
-  
-  headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {token}"
-  }
-
+  if not token: token = requests.post(f"{db_url}/v1/identity", json={}).json()["token"] 
+  headers = {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}
   response = requests.request(method, f"{db_url}{path}", headers=headers, data=body)
   return response
-
 
 @dataclass
 class Table:
@@ -41,9 +34,7 @@ def query_data(sql: str):
   rows = data[0]['rows']
   return Table(names, rows)
 
-
 def insert_data(prompt: str, schema: str, response: str, provider: str, model:str):
-
   body = json.dumps({
     "prompt": prompt,
     "schema": schema,
@@ -56,43 +47,49 @@ def insert_data(prompt: str, schema: str, response: str, provider: str, model:st
     print("ERROR:", ret.text)
     return
   else:
-    print("data inserted")
     return
 
+def model_query(prompt: str, schema: str, model: str, provider: str):
+  response = requests.post(
+    url = "https://openrouter.ai/api/v1/chat/completions",
+    headers = {
+      "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+    },
+    data = json.dumps({
+      "model": model,
+      "messages": [{"role" : "user","content" : prompt }],
+      "response_format": {"type": "json_schema", "json_schema": {"name": "response", "schema": schema}}
+    })
+  ).json()
+  resp = response
+  return resp['choices'][0]['message']['content']
 
-def query(prompt: str, schema: str, model = "python", provider = "python"):
-  cached = query_data(f"select * from llm_result where prompt = '{prompt}' and schema = '{schema}' and model = '{model}' and provider = '{provider}'")
+
+def query(prompt: str, schema: dict, model = "python", provider = "python"):
+  cached = query_data(f"select * from llm_result where prompt = '{prompt}' and schema = '{json.dumps(schema)}' and model = '{model}' and provider = '{provider}'")
   if cached.rows:
     return cached.rows[0][3]
   else:
-    response = '{"id": "some text"}'
-    insert_data(prompt, schema, response, provider, model)
+    response = model_query(prompt, schema, model, provider)
+    insert_data(prompt, json.dumps(schema), response, provider, model)
     return response
 
-    
-
-mini_schema = '''{
+mini_schema = {
   "type": "object",
   "properties": {
-    "id": { "type": "string" }
+    "contentr": { "type": "string" }
   },
-  "required": ["id"],
-  "additionalProperties": false
-}'''
+  "required": ["contentr"],
+  "additionalProperties": False
+}
 
 def main():
 
-  # insert_data("prompt", schema, '{"id": "some text"}', "python_template", "python_template")
-
-  # res =  query_data("select * from llm_result limit 100")
-
-  res = query("heyy2", mini_schema, "python", "python")
-
+  res = query("give a short string", mini_schema, "openai/gpt-4o", "openai")
   print(res)
 
 
-
-
 if __name__ == "__main__":
-  # asyncio.run(main())
   main()
+
+
