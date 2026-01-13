b5ed043883878db2ce0ffe25c4e1c45bfd653fb1
DKormann <dominikormann@outlook.com>
2026-01-13 13:24:50 +0100

    python requests

--- python_template/main.py
+++ python_template/main.py
@@ -1,74 +1,50 @@
-
-# const db_url = "https://maincloud.spacetimedb.com"
-# const body = document.body;
-
-# const DBNAME = "lexxtract"
-
-# let access_token = null;
-
-# async function setup(){
-#   await fetch(`${db_url}/v1/identity`, {
-#     method: 'POST',
-#     headers: {'Content-Type': 'application/json'},
-#   }).then(res=>res.json()).then(text=>access_token = text.token)
-
-# }
-
-# setup()
-
-# function add_data(prompt: string, schema: string, response: string, provider: string, model: string){
-#   const ajv = new Ajv();
-#   const validate = ajv.compile(JSON.parse(schema));
-#   const valid = validate(JSON.parse(response));
-#   if (!valid) {
-#     console.error("ERROR posting to database: ", validate.errors);  
-#     throw new Error(validate.errors ? validate.errors.map(e=>e.message).join(', ') : 'Invalid response');
-#   }
-
-#   fetch(`${db_url}/v1/database/${DBNAME}/call/add_call`, {
-#     method: 'POST',
-#     headers: {
-#       'Content-Type': 'application/json',
-#       'Authorization': `Bearer ${access_token}`
-#     },
-#     body: JSON.stringify({prompt, schema, response, provider, model})
-#   })
-
-#   .then(res=>res.text()).then(text=>{
-#     console.log(text)
-#   })
-# }
-
-# function query_data(sql: string){
-#   return fetch(`${db_url}/v1/database/${DBNAME}/sql`, {
-#     method: 'POST',
-#     headers: {
-#       'Content-Type': 'application/json',
-#       'Authorization': `Bearer ${access_token}`
-#     },
-#     body: sql
-#   })
-  
-#   .then(res=>{console.log(res); return res.json()}).then(data=>{
-#     if (data.length > 1) console.warn("multiple rows returned, TODO: handle this")
-#     let {schema, rows} = data[0]
-#     return {names: schema.elements.map(e=>e.name.some),rows}
-#   })
-#   .catch(e=>{console.error(e);
-#     popup(p(e.message))
-#     return {names: ["error"], rows: [e.message]}})
-# }
-
+from dataclasses import dataclass
 import requests
-
 import asyncio
 
 
-async def setup():
-  response = requests.post("https://maincloud.spacetimedb.com/v1/identity", json={})
-  data = response.json()["token"]
-  print(data)
+db_url = "https://maincloud.spacetimedb.com"
+dbname = "lexxtract"
 
+token : str = None
+
+async def db_request(path: str, method: str, body: str = None):
+  global token
+  if not token:
+    token = requests.post(f"{db_url}/v1/identity", json={}).json()["token"] 
+  
+  headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {token}"
+  }
+
+  response = requests.request(method, f"{db_url}{path}", headers=headers, data=body)
+  return response.json()
+
+
+@dataclass
+class Table:
+  names: list[str]
+  rows: list[list[any]]
+  def __repr__(self):
+    cols = len(self.names)
+    colw = 10
+    res = f'TABLE[{cols}x{len(self.rows)}]\n' + '-' * ((colw + 1) * cols + 1) + '\n'
+    for row in [self.names] + self.rows[:10]:
+      res += '|' + '|'.join(map(lambda x: str(x).replace('\n', '')[:colw].ljust(colw), row)) + '|\n'
+    return res
+
+async def query_data(sql: str):
+  data = await db_request(f"/v1/database/{dbname}/sql", "POST", body = sql.encode("utf-8"))
+  names = list(map(lambda x: x['name']['some'], data[0]['schema']['elements']))
+  rows = data[0]['rows']
+  return Table(names, rows)
+
+def main():
+  worker = query_data("select * from llm_result limit 100")
+
+  res = asyncio.run(worker)
+  print(res)
 
 if __name__ == "__main__":
-  asyncio.run(setup())
+  main()
